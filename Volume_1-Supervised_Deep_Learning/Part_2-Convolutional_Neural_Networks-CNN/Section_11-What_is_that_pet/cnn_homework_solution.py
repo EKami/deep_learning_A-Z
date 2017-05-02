0dbf46fb718dfb6d94509c7575ee02d5dc73c746
GODARD Tuatini <tuatinigodard@gmail.com>
2017-05-02 21:51:37 +0200

    Add my solution to the CNN homework

--- Volume_1-Supervised_Deep_Learning/Part_2-Convolutional_Neural_Networks-CNN/Section_11-What_is_that_pet/cnn_homework_solution.py
+++ Volume_1-Supervised_Deep_Learning/Part_2-Convolutional_Neural_Networks-CNN/Section_11-What_is_that_pet/cnn_homework_solution.py
@@ -17,66 +17,73 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+import os
+
+script_dir = os.path.dirname(__file__)
+training_set_path = os.path.join(script_dir, '../dataset/training_set')
+test_set_path = os.path.join(script_dir, '../dataset/test_set')
+file_test_path = os.path.join(script_dir, '../dataset/single_prediction/cat_or_dog_1.jpg')
 
 # Initialising the CNN
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 
 from keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
-training_set = train_datagen.flow_from_directory('dataset/training_set',
-                                                 target_size = (64, 64),
-                                                 batch_size = 32,
-                                                 class_mode = 'binary')
+training_set = train_datagen.flow_from_directory(training_set_path,
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
 
-test_set = test_datagen.flow_from_directory('dataset/test_set',
-                                            target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+test_set = test_datagen.flow_from_directory(test_set_path,
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+                         steps_per_epoch=8000,
+                         epochs=25,
+                         validation_data=test_set,
+                         validation_steps=2000)
 
 # Part 3 - Making new predictions
 
 import numpy as np
 from keras.preprocessing import image
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+
+test_image = image.load_img(file_test_path, target_size=(64, 64))
 test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
+test_image = np.expand_dims(test_image, axis=0)
 result = classifier.predict(test_image)
-training_set.class_indices
+print(training_set.class_indices)
 if result[0][0] == 1:
     prediction = 'dog'
 else:
