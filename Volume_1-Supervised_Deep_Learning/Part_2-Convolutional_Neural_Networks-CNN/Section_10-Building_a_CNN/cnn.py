e33bff310f0cc5bb1dd02a25e022f2f167ec3cae
Tuatini Godard <tuatinigodard@gmail.com>
2017-05-02 12:51:09 +0200

    Reformat code from CNN

--- Volume_1-Supervised_Deep_Learning/Part_2-Convolutional_Neural_Networks-CNN/Section_10-Building_a_CNN/cnn.py
+++ Volume_1-Supervised_Deep_Learning/Part_2-Convolutional_Neural_Networks-CNN/Section_10-Building_a_CNN/cnn.py
@@ -12,11 +12,13 @@
 # Part 1 - Building the CNN
 
 # Importing the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+from tensorflow.contrib.keras.api.keras.models import Sequential
+from tensorflow.contrib.keras.api.keras.layers import Conv2D
+from tensorflow.contrib.keras.api.keras.layers import MaxPooling2D
+from tensorflow.contrib.keras.api.keras.layers import Flatten
+from tensorflow.contrib.keras.api.keras.layers import Dense
+from tensorflow.contrib.keras.api.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.contrib.keras import backend
 import os
 
 script_dir = os.path.dirname(__file__)
@@ -27,48 +29,47 @@ test_set_path = os.path.join(script_dir, '../dataset/test_set')
 classifier = Sequential()
 
 # Step 1 - Convolution
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape=(64, 64, 3), activation='relu'))
 
 # Step 2 - Pooling
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Adding a second convolutional layer
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Conv2D(32, (3, 3), activation='relu'))
+classifier.add(MaxPooling2D(pool_size=(2, 2)))
 
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
-classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(units=128, activation='relu'))
+classifier.add(Dense(units=1, activation='sigmoid'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Part 2 - Fitting the CNN to the images
 
-from keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   shear_range = 0.2,
-                                   zoom_range = 0.2,
-                                   horizontal_flip = True)
-
-test_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 training_set = train_datagen.flow_from_directory(training_set_path,
-                                                 target_size = (64, 64),
-                                                 batch_size = 32,
-                                                 class_mode = 'binary')
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
 
 test_set = test_datagen.flow_from_directory(test_set_path,
-                                            target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
 
 classifier.fit_generator(training_set,
-                         steps_per_epoch = 8000,
-                         epochs = 25,
-                         validation_data = test_set,
-                         validation_steps = 2000)
+                         steps_per_epoch=8000,
+                         epochs=25,
+                         validation_data=test_set,
+                         validation_steps=2000)
+backend.clear_session()
