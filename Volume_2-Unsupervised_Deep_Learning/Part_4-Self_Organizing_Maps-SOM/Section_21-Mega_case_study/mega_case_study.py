5556a68de69b5508ac3e22bae1009c4ca4c41987
GODARD Tuatini <tuatinigodard@gmail.com>
2017-05-29 18:51:30 -0400

    Reorganize code

--- Volume_2-Unsupervised_Deep_Learning/Part_4-Self_Organizing_Maps-SOM/Section_21-Mega_case_study/mega_case_study.py
+++ Volume_2-Unsupervised_Deep_Learning/Part_4-Self_Organizing_Maps-SOM/Section_21-Mega_case_study/mega_case_study.py
@@ -8,25 +8,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 # Importing the dataset
-dataset = pd.read_csv('Credit_Card_Applications.csv')
+script_dir = os.path.dirname(__file__)
+training_set_path = os.path.join(script_dir, 'Credit_Card_Applications.csv')
+dataset = pd.read_csv(training_set_path)
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
-sc = MinMaxScaler(feature_range = (0, 1))
+
+sc = MinMaxScaler(feature_range=(0, 1))
 X = sc.fit_transform(X)
 
 # Training the SOM
 from minisom import MiniSom
-som = MiniSom(x = 10, y = 10, input_len = 15, sigma = 1.0, learning_rate = 0.5)
+
+som = MiniSom(x=10, y=10, input_len=15, sigma=1.0, learning_rate=0.5)
 som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
+som.train_random(data=X, num_iteration=100)
 
 # Visualizing the results
 from pylab import bone, pcolor, colorbar, plot, show
+
 bone()
 pcolor(som.distance_map().T)
 colorbar()
@@ -37,18 +43,16 @@ for i, x in enumerate(X):
     plot(w[0] + 0.5,
          w[1] + 0.5,
          markers[y[i]],
-         markeredgecolor = colors[y[i]],
-         markerfacecolor = 'None',
-         markersize = 10,
-         markeredgewidth = 2)
+         markeredgecolor=colors[y[i]],
+         markerfacecolor='None',
+         markersize=10,
+         markeredgewidth=2)
 show()
 
 # Finding the frauds
 mappings = som.win_map(X)
-frauds = np.concatenate((mappings[(5,3)], mappings[(8,3)]), axis = 0)
+frauds = np.concatenate((mappings[(5, 3)], mappings[(8, 3)]), axis=0)
 frauds = sc.inverse_transform(frauds)
-
-
 
 # Part 2 - Going from Unsupervised to Supervised Deep Learning
 
@@ -58,11 +62,12 @@ customers = dataset.iloc[:, 1:].values
 # Creating the dependent variable
 is_fraud = np.zeros(len(dataset))
 for i in range(len(dataset)):
-    if dataset.iloc[i,0] in frauds:
+    if dataset.iloc[i, 0] in frauds:
         is_fraud[i] = 1
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
+
 sc = StandardScaler()
 customers = sc.fit_transform(customers)
 
@@ -76,18 +81,18 @@ from keras.layers import Dense
 classifier = Sequential()
 
 # Adding the input layer and the first hidden layer
-classifier.add(Dense(units = 2, kernel_initializer = 'uniform', activation = 'relu', input_dim = 15))
+classifier.add(Dense(units=2, kernel_initializer='uniform', activation='relu', input_dim=15))
 
 # Adding the output layer
-classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
 # Compiling the ANN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # Fitting the ANN to the Training set
-classifier.fit(customers, is_fraud, batch_size = 1, epochs = 2)
+classifier.fit(customers, is_fraud, batch_size=1, epochs=2)
 
 # Predicting the probabilities of frauds
 y_pred = classifier.predict(customers)
-y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis = 1)
+y_pred = np.concatenate((dataset.iloc[:, 0:1].values, y_pred), axis=1)
 y_pred = y_pred[y_pred[:, 1].argsort()]
